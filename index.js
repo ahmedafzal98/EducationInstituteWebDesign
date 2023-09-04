46ac8caf498c54b85798059b5484a6dfa55ef94d
ahmedafzal98 <ahmed.afzal2070@gmail.com>
2023-09-05 00:39:09 +0500

    add hamburger menu

---
+++ index.js
@@ -0,0 +1,14 @@
+document.addEventListener("DOMContentLoaded",()=>{
+    let icon = document.getElementById("icon");
+    icon.addEventListener("click",(event)=>{
+        event.preventDefault(); 
+       let nav = document.getElementById("top-nav")
+       if (nav.className === 'navbar') {
+        nav.className += 'responsive'
+       }else{
+        nav.className = 'navbar'
+       }
+
+        console.log(nav);
+    })
+})
